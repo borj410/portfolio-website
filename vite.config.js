be77e516033ef51b719eb8a3dc811e86bce14f7c
Gabriel Borja <borjagabriel45@gmail.com>
2026-03-16 16:37:55 -0500

    Deploy portfolio page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/borj410/portfolio-website',
+  base: 'https://borj410.github.io/portfolio-website/',
 })
