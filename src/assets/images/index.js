5f7cd412c47d2627c1c1e45da0e0f68f8793cb0b
Gabriel Borja <borjagabriel45@gmail.com>
2026-03-20 14:10:04 -0500

    Update image assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,6 @@
 export { default as LivriaImage } from "./Livria.png";
 export { default as TalentManagerImage } from "./TalentManager.jpg";
 export { default as PokemonMobileImage } from "./PokemonMobile.png";
-export { default as PortfolioOSImage } from "./Portfolio.png";
 
 export { default as Detective1 } from "./detective_eye.png";
 export { default as Detective2 } from "./detective_noeye.png";
